2ab5739412afa263d785687ca2d523bab0f21eca
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2026-06-06 22:11:16 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://placeholder-project.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "placeholder-anon-key";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://rcdtppxxagtqjtacklrz.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjZHRwcHh4YWd0cWp0YWNrbHJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NTM3MjQsImV4cCI6MjA5NjMyOTcyNH0.7PjvbsB1KSYmFLa4_0q80VvFAAD436nT4LsQRbiMq2M";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
